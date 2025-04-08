4b8a67823be556f75ca1987e777c7f6777f221be
payal jain <payaljain.014@gmail.com>
2025-04-09 01:35:15 +0530

    add failing test for add (class)function which is not exist.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,4 +2,11 @@ require 'rspec'
 require "string_calculator"
 
 describe StringCalculator do
+  describe ".add" do
+    context "given an empty string" do
+      it "returns zero" do
+        expect(StringCalculator.add("")).to eq(0)
+      end
+    end
+  end
 end
