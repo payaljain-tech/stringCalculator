f1ca2668e11eeb2692eb0b0da96880f30e020eed
payal jain <payaljain.014@gmail.com>
2025-04-09 01:47:54 +0530

    Added failing test for - to pass two inputs seperated by commas.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,10 +9,25 @@ describe StringCalculator do
       end
     end
 
-    context "given '7'" do
+    context "given single input" do
       it "returns 7" do
         expect(StringCalculator.add("7")).to eql(7)
       end
     end
+
+    context "two numbers" do
+      context "given '2,4'" do
+        it "returns 6" do
+          expect(StringCalculator.add("2,4")).to eql(6)
+        end
+      end
+
+      context "given '17,100'" do
+        it "returns 117" do
+          expect(StringCalculator.add("17,10")).to eql(27)
+        end
+      end
+    end
   end
 end
+
