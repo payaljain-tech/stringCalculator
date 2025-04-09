ac5cc967794424cb45821aaead3deedfd149443b
payal jain <payaljain.014@gmail.com>
2025-04-09 14:26:08 +0530

    implemented code to pass different elimeters in input strings.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,7 +4,7 @@ class StringCalculator
     if string.empty?
       0
     else
-      numbers = string.gsub("\n",",").split(",").map { |num| num.to_i }
+      numbers = string.gsub(/[^-100-9]/, ',').split(",").map { |num| num.to_i }
       numbers.inject(0) { |sum, number| sum + number }
     end
   end
